7d1e9ca68c69bdc512079fe62f5abe29b6749c52
wictorHead <wictormusic1@gmail.com>
2025-06-17 14:46:24 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-import express from "express";
-import axios from "axios";
+const express = require("express");
+const axios = require("axios");
 
 const app = express();
 app.use(express.json());
 
-const TOKEN_MP = "APP_USR-8581410906528367-061713-ca153ee00cb409a001997f2e20d896da-2448358894; 
+const TOKEN_MP = "APP_USR-8581410906528367-061713-ca153ee00cb409a001997f2e20d896da-2448358894";
 
 app.post("/webhook", async (req, res) => {
   const paymentId = req.body.data?.id;
@@ -18,7 +18,7 @@ app.post("/webhook", async (req, res) => {
     });
 
     if (pagamento.status === "approved") {
-      await axios.post("https://SEUSITE.wixsite.com/_functions/atualizarStatus", {
+      await axios.post("https://headcase.com.br/_functions/atualizarStatus", {
         idPagamento: pagamento.id
       });
     }
